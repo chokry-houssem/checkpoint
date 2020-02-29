fd81ac3de86264222209bf38aa533402187d6a5d
houssemchokri <61630874+houssemchokri@users.noreply.github.com>
2020-02-29 21:22:17 +0100

    Add files via upload

---
+++ gomycode/js/main.js
@@ -0,0 +1,20 @@
+function gra()
+{
+	document.getElementById("test").style.fontWeight = "900";
+}
+function font()
+{
+	document.getElementById("test").style.fontFamily = "cursive,Charcoal,Impact";
+}
+function myItalic()
+{
+	document.getElementById("test").style.fontStyle = "italic";
+}
+function fontS()
+{
+	document.getElementById("test").style.fontSize = "2em";
+}
+function textUN()
+{
+	document.getElementById("test").style.textDecoration = "underline";
+}
